d4c2d393ec9fe43dc696be27478ca01ef86adf11
Tim Dronkers <tdronkers@gmail.com>
2022-07-28 08:52:49 -0600

    going with status code 204 for successful PUT req
    Co-authored-by: Katarina Rosiak <katarinarosiak@gmail.com>
    Co-authored-by: Miles Abbason <miles.abbason@gmail.com>
    Co-authored-by: Scott Graham <scttgrhm7+public@gmail.com>

--- controllers/testsController.js
+++ controllers/testsController.js
@@ -153,17 +153,17 @@ const getTestRun = async (req, res) => {
 };
 
 const editTest = async (req, res) => {
-  // const errors = validationResult(req);
-  // if (errors.isEmpty()) {
-  //   try {
-  //     await createEventBridgeRule(req.body);
-  //     res.status(201).send(`Test ${req.body.test.title} created`);
-  //   } catch (err) {
-  //     console.log('Error: ', err);
-  //   }
-  // } else {
-  //   res.status(400).json({ errors: errors.array() });
-  // }
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    try {
+      // await createEventBridgeRule(req.body);
+      res.status(204).send(`Test ${req.body.test.title} updated`);
+    } catch (err) {
+      console.log('Error: ', err);
+    }
+  } else {
+    res.status(400).json({ errors: errors.array() });
+  }
 };
 
 exports.runNow = runNow;
