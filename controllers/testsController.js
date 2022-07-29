e353e778fde18b509ea5699418d2dd847a806fe3
Tim Dronkers <tdronkers@gmail.com>
2022-07-29 10:35:03 -0600

    remove duplicate editTest function in testControllers Co-authored-by: Katarina Rosiak <katarinarosiak@gmail.com> Co-authored-by: Miles Abbason <miles.abbason@gmail.com> Co-authored-by: Scott Graham <scttgrhm7+public@gmail.com>

--- controllers/testsController.js
+++ controllers/testsController.js
@@ -229,21 +229,6 @@ const deleteTest = async (req, res) => {
   }
 };
 
-const editTest = async (req, res) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    const testId = req.params.id;
-    try {
-      await editEventBridgeRule(testId, req.body);
-      res.status(200).send(`Test ${req.body.test.title} updated`);
-    } catch (err) {
-      console.log('Error: ', err);
-    }
-  } else {
-    res.status(400).json({ errors: errors.array() });
-  }
-};
-
 exports.runNow = runNow;
 exports.createTest = createTest;
 exports.getScheduledTests = getScheduledTests;
